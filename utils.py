59cbb80cda3aea7a0a27ffc23b5599e56eb35de3
Slash228 <lawlliieett@gmail.com>
2025-06-19 20:21:11 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import random
-
-def load_yaml(path):
-    with open(path, "r") as f:
-        return yaml.safe_load(f)
-
-def choose_random_command(commands):
-    return random.choice(commands)
